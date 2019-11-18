c753a71c4f4b2f5fad86de2da012a9aa25819e4e
Andrea <35015674+Sgridev@users.noreply.github.com>
2019-11-18 18:42:37 +0100

    now she can be beautiful up to 10k
    now with 100% less binded public credentials

--- Program.cs
+++ Program.cs
@@ -1,26 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TweetSharp;
 using static System.Net.Mime.MediaTypeNames;
-
+//now with 100% less binded public credentials
 namespace twitterBot1
 {
     class Program
     {
-        private static  string customer_key = "3wxC4PHtaE2Yzq5LaBKop0bsJ";
-        private static  string customer_key_secret = "yMF5kFNZ2CfEoHfjluSaz1m4WLmrDwWJhZmco2ebxHx0Mvtm5K";
-        private static  string access_token = "1158289304612986880-05SCqmpAV4reqZDXv2wiH3YoE2v0Mu";
-        private static  string access_token_secret = "tE1XMvCMOoQW21tdLuVqfYtwcmJLGxe2gF9QSQ2QTNhQN";
-   
+        private static string customer_key = ConfigurationManager.AppSettings["customer_key"];
+        private static  string customer_key_secret = ConfigurationManager.AppSettings["customer_key_secret"];
+        private static  string access_token = ConfigurationManager.AppSettings["access_token"];
+        private static  string access_token_secret = ConfigurationManager.AppSettings["access_token_secret"];
+      
         private static TwitterService service = new TwitterService(customer_key, customer_key_secret, access_token, access_token_secret);
         private static Random random = new Random();
         static void Main(string[] args)
         {
             Console.WriteLine($"<{DateTime.Now}> - Bot Started");
-            SendTweet("Daily reminder: Sara è bellissima " + random.Next(1000).ToString());
+            SendTweet("Daily reminder: Sara è bellissima " + random.Next(10000).ToString());
             Console.Read();
         }
 
